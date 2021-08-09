01a2a7eaaaba3f018d7ebd57b77a18c9da263978
Özgür Özdemir <ozgur.96.ozdemir@gmail.com>
2021-08-09 16:41:45 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function trans(obj) {
   $('#LA').fadeOut(400, function() {
-    $(this).html('<p>“I doubt therefore I think,<br><span style="padding-left: 2em;">I think therefore I am.”</span></p>').fadeIn(200);
+    $(this).html('<p>“I think therefore I am.”</p>').fadeIn(200);
     $(this).attr("id", "EN");
   });
 }
@@ -8,7 +8,7 @@ function trans(obj) {
 
 function transBack(obj) {
   $('#EN').fadeOut(400, function() {
-    $(this).html('<p>“Dubito ergo cogito,<br><span style = "padding-left: 2em;"> cogito ergo sum.” </span></p> ').fadeIn(200);
+    $(this).html('<p>“Cogito ergo sum.”</p> ').fadeIn(200);
     $(this).attr("id", "LA");
   });
 }
